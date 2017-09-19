81b573cb368abb65630a9e6471153a522438c5af
Stepan Romashkov <akumajo27@gmail.com>
2017-09-19 11:15:18 -0400

    the second and the only

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /* The next part is just for fun. It's boring just to write
+             * the same "Hello World!" again and again, isn't it?
+             */
+
+            Console.WriteLine("\n");
+
+            String helloWorld = "Hello Beautiful World!\n\n";
+            String word = "";
+            int position = 0;
+            Console.WriteLine(helloWorld);
+
+            do
+            {
+                if (helloWorld.ElementAt(position) != '!' && helloWorld.ElementAt(position) != ' ')
+                {
+                    word += helloWorld.ElementAt(position);
+                    position++;
+                }
+                else
+                {
+                    Console.WriteLine(word + "!\n");
+                    word = "";
+                    position++;
+                }
+            }
+            while (position < helloWorld.Length);
+
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
